c50ca623650be2911184fa21f001776d711b8f6f
Katherine Zaoral <kz@adhoc.com.ar>
2019-04-24 15:06:23 -0300

    [ADD] l10n_ar_account: delete this module because if was merged into l10n_ar

--- addons/l10n_ar_account/__init__.py
+++ addons/l10n_ar_account/__init__.py
@@ -2,5 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import report
 from .hooks import post_init_hook
