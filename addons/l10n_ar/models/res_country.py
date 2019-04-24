08ec9d748e9ac6c1e59a7a9758798edaea8ba9df
Katherine Zaoral <kz@adhoc.com.ar>
2019-04-24 14:45:03 -0300

    [ADD] l10n_ar: move res.country model from l10n_ar_account model
    also remove the cuit_fisica and cuit_juridica this two fields were already
    added in l10_ar_base

--- addons/l10n_ar/models/res_country.py
+++ addons/l10n_ar/models/res_country.py
@@ -6,17 +6,10 @@ from odoo import fields, models
 
 
 class ResCountry(models.Model):
+
     _inherit = 'res.country'
 
     afip_code = fields.Char(
         'Afip Code',
         size=3,
-    )
-    cuit_fisica = fields.Char(
-        'CUIT persona fisica',
-        size=11,
-    )
-    cuit_juridica = fields.Char(
-        'CUIT persona juridica',
-        size=11,
     )
