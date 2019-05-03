62cf7f71ee2ddc206dccf4d653099ee92de9e4ef
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-03 12:11:37 -0300

    [FIX] account_document: rename l10n_latam_account_document_type to l10n_latam_document_type

--- addons/account_document/models/__init__.py
+++ addons/account_document/models/__init__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import res_company
-from . import l10n_latam_account_document_type
+from . import l10n_latam_document_type
 from . import l10n_latam_account_journal_document_type
 from . import l10n_latam_account_payment_receiptbook
 from . import account_journal
