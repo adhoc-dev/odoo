9acaaa31740fb2b453f1650af3b9c30e06ba4404
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-03 13:30:43 -0300

    [FIX] account_document: account_payment_view

--- addons/account_document/__manifest__.py
+++ addons/account_document/__manifest__.py
@@ -16,7 +16,7 @@
         'views/l10n_latam_document_type_view.xml',
         'views/account_invoice_view.xml',
         'views/report_invoice.xml',
-        # 'views/account_payment_view.xml',
+        'views/account_payment_view.xml',
         # 'views/account_payment_receiptbook_view.xml',
         # 'views/menuitem.xml',
         # 'views/account_portal_templates.xml',
