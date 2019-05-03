387a3904e5f10373586d0d70b481de144de99479
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-03 11:36:26 -0300

    [REF] base_validator: integrate module description from README file to manifest

--- addons/base_validator/__manifest__.py
+++ addons/base_validator/__manifest__.py
@@ -6,6 +6,17 @@
     'summary': '',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
+    'description': """
+This module allows to set a python expression as value validator.
+This module is intended to be extended by others and should not be installed
+alone.
+
+To use this module, you need to:
+
+* Depend on this module
+* Create new records for new validations
+* Call validation from where you need
+""",
     'depends': [
     ],
     'data': [
