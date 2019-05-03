14ed298342d7b40c43ba6245e903cfa9d9275c70
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-03 13:25:16 -0300

    [REM] account_document: deleted views res_company_view and res_partner_view

--- addons/account_document/__manifest__.py
+++ addons/account_document/__manifest__.py
@@ -15,8 +15,6 @@
         'views/account_move_view.xml',
         'views/l10n_latam_document_type_view.xml',
         'views/account_invoice_view.xml',
-        # 'views/res_company_view.xml',
-        # 'views/res_partner_view.xml',
         'views/report_invoice.xml',
         # 'views/account_chart_template_view.xml',
         # 'views/account_payment_view.xml',
