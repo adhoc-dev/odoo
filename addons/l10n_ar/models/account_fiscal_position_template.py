07ad82d69ea2280b62186ed201a3f6a03145d1e4
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-05 15:42:44 -0300

    [FIX] l10n_ar: account.fiscal.position.template
    * remove fields already defined in odoo core (account module)
    * pretty print related view

--- addons/l10n_ar/models/account_fiscal_position_template.py
+++ addons/l10n_ar/models/account_fiscal_position_template.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 
 
 class AccountFiscalPositionTemplate(models.Model):
+
     _inherit = 'account.fiscal.position.template'
 
     l10n_ar_afip_code = fields.Char(
@@ -10,20 +11,3 @@ class AccountFiscalPositionTemplate(models.Model):
         help='For eg. This code will be used on electronic invoice and citi '
         'reports',
     )
-
-    # TODO this fields should be added on odoo core
-    auto_apply = fields.Boolean(
-        string='Detect Automatically',
-        help="Apply automatically this fiscal position.")
-    country_id = fields.Many2one(
-        'res.country', string='Country',
-        help="Apply only if delivery or invoicing country match.")
-    country_group_id = fields.Many2one(
-        'res.country.group', string='Country Group',
-        help="Apply only if delivery or invocing country match the group.")
-    state_ids = fields.Many2many(
-        'res.country.state', string='Federal States')
-    zip_from = fields.Integer(
-        string='Zip Range From', default=0)
-    zip_to = fields.Integer(
-        string='Zip Range To', default=0)
