e64f8ce9de118fce2e0ee328d8e75a9b986bc0a4
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-05 15:05:15 -0300

    [FIX] l10n_ar: add _description to afip.vat.f2002_category model

--- addons/l10n_ar/models/afip_vat_f2002_category.py
+++ addons/l10n_ar/models/afip_vat_f2002_category.py
@@ -2,7 +2,9 @@ from odoo import models, fields
 
 
 class AfipVatF2002Category(models.Model):
+
     _name = "afip.vat.f2002_category"
+    _description = "AFIP VAT F2002 Category"
 
     name = fields.Char(
         required=True,
