a9f719216cdb21a01a3973bd2241f9ebbf4a9a2c
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-05 13:54:09 -0300

    [FIX] l10n_ar: delete afip_activity views security and fields
    this model was decided to be deleted

--- addons/l10n_ar/models/account_account.py
+++ addons/l10n_ar/models/account_account.py
@@ -7,12 +7,6 @@ class AccountAccount(models.Model):
 
     _inherit = 'account.account'
 
-    afip_activity_id = fields.Many2one(
-        'afip.activity',
-        'AFIP Activity',
-        help='AFIP activity, used for IVA f2002 report',
-        auto_join=True,
-    )
     vat_f2002_category_id = fields.Many2one(
         'afip.vat.f2002_category',
         auto_join=True,
