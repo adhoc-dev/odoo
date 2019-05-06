e27773f5272be401fd7d382a4891e6854ec02626
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-06 13:13:46 -0300

    [ADD] l10n_ar: make get_fiscal_position work with new logic

--- addons/l10n_ar/models/account_fiscal_position.py
+++ addons/l10n_ar/models/account_fiscal_position.py
@@ -13,24 +13,30 @@ class AccountFiscalPosition(models.Model):
     )
 
     def get_fiscal_position(self, partner_id, delivery_id=None):
-        PartnerObj = self.env['res.partner']
-        partner = PartnerObj.browse(partner_id)
-        afip_responsability = (
-            partner.commercial_partner_id.l10n_ar_afip_responsability_type)
-        # para nosotros fp con vat_required o no es segun responsabilidad y no
-        # segun si el partner tiene seteado vat
-
-        # if it is exento, proveedor del exterior or monotributo, when we buy
-        # there are no taxes so we choose a vat_required=False position
-        # (IVA Compras no corresponde)
-        if afip_responsability in ['6', '4', '8']:
-            # if no delivery use invoicing
-            if delivery_id:
-                delivery = PartnerObj.browse(delivery_id)
-            else:
-                delivery = partner
-            fp = self._get_fpos_by_region(
-                delivery.country_id.id, delivery.state_id.id, delivery.zip,
-                vat_required=False)
-            return fp.id if fp else False
+        """ Fiscal position does not depends on the partner vat, it depends on
+        the partner AFIP responsability
+        """
+        company_id = self._context.get(
+            'force_company', self.env.user.company_id.id)
+        if self.env['res.company'].browse(company_id).country_id.code == 'AR':
+            partner = self.env['res.partner'].browse(partner_id)
+            afip_responsability = \
+                partner.commercial_partner_id.l10n_ar_afip_responsability_type
+            self = self.with_context(
+                partner_afip_responsability=afip_responsability)
         return super().get_fiscal_position(partner_id, delivery_id=delivery_id)
+
+    @api.model
+    def _get_fpos_by_region(
+        self, country_id=False, state_id=False, zipcode=False,
+        vat_required=False):
+        """ Take into account the partner afip responsability in order to
+        now if it is vat required or not
+        """
+        if 'partner_afip_responsability' in self._context:
+            vat_required = self._context.get(
+                'partner_afip_responsability') not in ['6', '4', '8']
+
+        return super()._get_fpos_by_region(
+            country_id=country_id, state_id=state_id, zipcode=zipcode,
+            vat_required=vat_required)
