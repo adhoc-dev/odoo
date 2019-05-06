f143d468be9f1fa0bf49a08368365598b4d8831f
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-06 15:06:19 -0300

    [REF] l10n_ar: remove load_translations this is not needed.
    This is only required for localization modules with i18n_extra

--- addons/l10n_ar/__init__.py
+++ addons/l10n_ar/__init__.py
@@ -3,10 +3,3 @@
 from . import models
 from . import wizards
 from .hooks import post_init_hook
-
-from odoo import SUPERUSER_ID
-
-def load_translations(cr, registry):
-    chart_template = registry['ir.model.data'].xmlid_to_object(
-        cr, SUPERUSER_ID, 'l10n_ar.l10nar_chart_template')
-    chart_template.process_coa_translations()
