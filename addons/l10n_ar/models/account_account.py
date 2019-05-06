3b2c1f30a3bb4c9ecf0714fe6b82211bb79a4941
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-06 14:53:23 -0300

    [FIX] l10n_ar: references to renamed module was wrong, update to new one

--- addons/l10n_ar/models/account_account.py
+++ addons/l10n_ar/models/account_account.py
@@ -7,7 +7,7 @@ class AccountAccount(models.Model):
     _inherit = 'account.account'
 
     l10n_ar_vat_f2002_category_id = fields.Many2one(
-        'afip.vat.f2002.category',
+        'l10n_ar.afip.vat.f2002.category',
         auto_join=True,
         string='Categoría IVA f2002',
     )
