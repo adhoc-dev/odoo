5bbd085a2bbbee99278802ec7f90d8a8a5a49a96
Katherine Zaoral <kz@adhoc.com.ar>
2020-07-22 10:45:54 -0300

    [TEST][IMP] l10n_ar_website_sale: extend _get_vat_validation_fields to add l10n_latam_identification_type_id field

--- addons/l10n_ar_website_sale/controllers/main.py
+++ addons/l10n_ar_website_sale/controllers/main.py
@@ -24,21 +24,9 @@ class L10nARWebsiteSale(WebsiteSale):
                                       'responsibility': kw.get('l10n_ar_afip_responsibility_type_id')})
         return response
 
-    def _vat_validation(self, data, error, error_message):
-        """ Do the vat validation taking into account the identification_type """
+    def _get_vat_validation_fields(self, data):
+        res = super()._get_vat_validation_fields(data)
         if request.website.sudo().company_id.country_id == request.env.ref('base.ar'):
-            Partner = request.env['res.partner']
-            if data.get("vat") and hasattr(Partner, "check_vat"):
-                partner_dummy = Partner.new({
-                    'vat': data['vat'],
-                    'country_id': (int(data['country_id']) if data.get('country_id') else False),
-                    'l10n_latam_identification_type_id': (int(data['l10n_latam_identification_type_id'])
-                                                          if data.get('l10n_latam_identification_type_id') else False),
-                })
-                try:
-                    partner_dummy.check_vat()
-                except ValidationError as exception:
-                    error["vat"] = 'error'
-                    error_message.append(exception.name)
-            return error, error_message
-        return super()._vat_validation(data, error, error_message)
+            res.update({'l10n_latam_identification_type_id': int(data['l10n_latam_identification_type_id'])
+                                                             if data.get('l10n_latam_identification_type_id') else False})
+        return res
