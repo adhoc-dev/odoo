71288cb9b13cd27ec5fc5ea8834721c980a52cdf
Katherine Zaoral <kz@adhoc.com.ar>
2020-07-23 10:22:26 -0300

    [REF] l10n_ar_website_sale: use string compare in condition instead of id compare

--- addons/l10n_ar_website_sale/controllers/main.py
+++ addons/l10n_ar_website_sale/controllers/main.py
@@ -9,7 +9,7 @@ class L10nARWebsiteSale(WebsiteSale):
     def _get_mandatory_billing_fields(self):
         """Extend mandatory fields to add new identification and responsibility fields when company is argentina"""
         res = super()._get_mandatory_billing_fields()
-        if request.website.sudo().company_id.country_id == request.env.ref('base.ar'):
+        if request.website.sudo().company_id.country_id.code == "AR":
             res += ["l10n_latam_identification_type_id", "l10n_ar_afip_responsibility_type_id", "vat"]
         return res
 
@@ -17,7 +17,7 @@ class L10nARWebsiteSale(WebsiteSale):
     def address(self, **kw):
         """Extend to send information about the identification types and AFIP responsibility to show in the address form"""
         response = super().address(**kw)
-        if request.website.sudo().company_id.country_id == request.env.ref('base.ar'):
+        if request.website.sudo().company_id.country_id.code == "AR":
             response.qcontext.update({'identification_types': request.env['l10n_latam.identification.type'].sudo().search([]),
                                       'responsibility_types': request.env['l10n_ar.afip.responsibility.type'].sudo().search([]),
                                       'identification': kw.get('l10n_latam_identification_type_id'),
@@ -26,7 +26,7 @@ class L10nARWebsiteSale(WebsiteSale):
 
     def _get_vat_validation_fields(self, data):
         res = super()._get_vat_validation_fields(data)
-        if request.website.sudo().company_id.country_id == request.env.ref('base.ar'):
+        if request.website.sudo().company_id.country_id.code == "AR":
             res.update({'l10n_latam_identification_type_id': int(data['l10n_latam_identification_type_id'])
                                                              if data.get('l10n_latam_identification_type_id') else False})
         return res
