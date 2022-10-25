5118e5e98b1f9707ccc44dff947efa5566607dad
Katherine Zaoral <kz@adhoc.com.ar>
2022-10-25 15:19:26 -0300

    [ADD] l10n_latam_check: Checks Management
    New features to manage checks and managed them Odoo in latam countries
    where they are widely use.
    
    Own Checks Management
    ---------------------
    
    Extends 'Check Printing Base' module to manage own checks with more features:
    
    * allow using own checks that are not printed but filled manually by the user
    * allow to use deferred or electronic checks
      * printing is disabled
      * check number is set manually by the user
    * add an optional "Check Cash-In Date" for post-dated checks (deferred payments)
    * add a menu to track own checks
    
    Third Party Checks Management
    -----------------------------
    
    Add new "Third party check Management" feature.
    
    There are 2 main Payment Methods additions:
    
    * New Third Party Checks:
    
       * allow the user create a check on the fly from a payment
       * create a third party check from a customer payment
    
    * Existing Third party check:
    
       * allow the user to reuse a Third party check already created
       * pay a vendor bill using an existing Third party check
       * move an existing checks between journals (i.e. move to Rejected)
       * Send/Receive again a check already used in a Vendor Bill/Customer INV
       * allow the user to do mass check transfers

---
+++ addons/l10n_latam_check/__manifest__.py
@@ -0,0 +1,60 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Advanced Check Management',
+    'version': "1.0.0",
+    'category': 'Accounting/Localizations',
+    'summary': 'Checks Management',
+    'description': """
+Own Checks Management
+---------------------
+
+Extends 'Check Printing Base' module to manage own checks with more features:
+
+* allow using own checks that are not printed but filled manually by the user
+* allow to use deferred or electronic checks
+  * printing is disable
+  * check number is set manually by the user
+* add an optional "payment date" for post-dated checks (deferred payments)
+* add a menu to track own checks
+
+Third Party Checks Management
+-----------------------------
+
+Add new "Third party check Management" feature.
+
+There are 2 main Payment Methods additions:
+
+* New Third Party Checks:
+
+   * allow the user create a check on the fly from a payment
+   * create a third party check from a customer payment
+
+* Existing Third party check:
+
+   * allow the user to reuse a Third party check already created
+   * pay a vendor bill using an existing Third party check
+   * move an existing checks between journals (i.e. move to Rejected)
+   * Send/Receive again a check already used in a Vendor Bill/Customer INV
+   * allow the user to do mass check transfers
+
+""",
+    'author': 'ADHOC SA',
+    'license': 'LGPL-3',
+    'images': [
+    ],
+    'depends': [
+        'account_check_printing',
+        'base_vat',
+    ],
+    'data': [
+        'data/account_payment_method_data.xml',
+        'security/ir.model.access.csv',
+        'views/account_payment_view.xml',
+        'views/account_journal_view.xml',
+        'wizards/account_payment_register_views.xml',
+        'wizards/l10n_latam_payment_mass_transfer_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ addons/l10n_latam_check/tests/test_own_checks.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.addons.l10n_latam_check.tests.common import L10nLatamCheckTest
+from odoo.tests.common import tagged
+
+
+@tagged('post_install_l10n', 'post_install', '-at_install')
+class TestOwnChecks(L10nLatamCheckTest):
+
+    def test_01_pay_with_multiple_checks(self):
+        """ Create one check with each checkbook, first check should choose deferred check by default. On current
+        check force a different number than next one"""
+        vals_list = [{
+            'ref': 'Deferred check',
+            'partner_id': self.partner_a.id,
+            'amount': '00000001',
+            'check_number': '50',
+            'payment_type': 'outbound',
+            'journal_id': self.bank_journal.id,
+            'payment_method_line_id': self.bank_journal._get_available_payment_method_lines('outbound').filtered(lambda x: x.code == 'check_printing').id,
+        }]
+        payment = self.env['account.payment'].create(vals_list)
+        payment.action_post()
+        self.assertTrue(payment, 'Checks where not created properly')
+        self.assertRecordValues(payment, [{
+            'state': 'posted',
+            'is_move_sent': True,
+        }])
