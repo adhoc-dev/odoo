9cdfe05b61760cc134032020db0cc823e800f574
Katherine Zaoral <kz@adhoc.com.ar>
2022-11-10 10:35:16 -0300

    * remove checks from manifest, messages in the code and unit tests

--- addons/l10n_latam_check/tests/common.py
+++ addons/l10n_latam_check/tests/common.py
@@ -19,24 +19,6 @@ class L10nLatamCheckTest(AccountTestInvoicingCommon):
         cls.bank_journal = cls.company_data_3['default_journal_bank']
         # enable checkbooks on bank journal
         cls.bank_journal.l10n_latam_use_checkbooks = True
-        cls.deferred_checkbook = cls.env['l10n_latam.checkbook'].create({
-            'journal_id': cls.bank_journal.id,
-            'next_number': 50,
-            'range_to': 100,
-            'type': 'deferred',
-        })
-        cls.current_checkbook = cls.env['l10n_latam.checkbook'].create({
-            'journal_id': cls.bank_journal.id,
-            'next_number': 100,
-            'range_to': 150,
-            'type': 'currents',
-        })
-        cls.electronic_checkbook = cls.env['l10n_latam.checkbook'].create({
-            'journal_id': cls.bank_journal.id,
-            'next_number': 200,
-            'type': 'electronic',
-        })
-
         third_party_checks_journals = cls.env['account.journal'].search([('outbound_payment_method_line_ids.code', '=', 'new_third_party_checks'), ('inbound_payment_method_line_ids.code', '=', 'out_third_party_checks'), ('inbound_payment_method_line_ids.code', '=', 'new_third_party_checks')])
         cls.third_party_check_journal = third_party_checks_journals[0]
         cls.rejected_check_journal = third_party_checks_journals[1]
