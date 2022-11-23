4a83947e3731401e70fc0cf7d992747b408ed994
Katherine Zaoral <kz@adhoc.com.ar>
2022-11-23 20:45:52 -0300

    separete _compute_l10n_latam_check_data compute in two

--- addons/l10n_latam_check/models/account_payment.py
+++ addons/l10n_latam_check/models/account_payment.py
@@ -42,12 +42,12 @@ class AccountPayment(models.Model):
     l10n_latam_check_bank_id = fields.Many2one(
         comodel_name='res.bank',
         string='Check Bank',
-        compute='_compute_l10n_latam_check_data', store=True, readonly=False,
+        compute='_compute_l10n_latam_check_bank_id', store=True, readonly=False,
         states={'posted': [('readonly', True)], 'cancel': [('readonly', True)]},
     )
     l10n_latam_check_issuer_vat = fields.Char(
         string='Check Issuer VAT',
-        compute='_compute_l10n_latam_check_data', store=True, readonly=False,
+        compute='_compute_l10n_latam_check_issuer_vat', store=True, readonly=False,
         states={'posted': [('readonly', True)], 'cancel': [('readonly', True)]},
     )
     l10n_latam_check_payment_date = fields.Date(
@@ -92,11 +92,18 @@ class AccountPayment(models.Model):
         return super(AccountPayment, self - avoid_inverse)._inverse_check_number()
 
     @api.depends('payment_method_line_id.code', 'partner_id')
-    def _compute_l10n_latam_check_data(self):
+    def _compute_l10n_latam_check_bank_id(self):
         new_third_party_checks = self.filtered(lambda x: x.payment_method_line_id.code == 'new_third_party_checks')
         for rec in new_third_party_checks:
             rec.l10n_latam_check_bank_id = rec.partner_id.bank_ids[:1].bank_id
+        (self - new_third_party_checks).l10n_latam_check_bank_id = False
+
+    @api.depends('payment_method_line_id.code', 'partner_id')
+    def _compute_l10n_latam_check_issuer_vat(self):
+        new_third_party_checks = self.filtered(lambda x: x.payment_method_line_id.code == 'new_third_party_checks')
+        for rec in new_third_party_checks:
             rec.l10n_latam_check_issuer_vat = rec.partner_id.vat
+        (self - new_third_party_checks).l10n_latam_check_issuer_vat = False
 
     @api.onchange('l10n_latam_check_issuer_vat')
     def _clean_l10n_latam_check_issuer_vat(self):
