29218c559c3b226d401ec1f8445ddb759ce2d4f1
Katherine Zaoral <kz@adhoc.com.ar>
2022-11-30 15:11:36 -0300

    improve/simpligy default_get method using good practices

--- addons/l10n_latam_check/wizards/l10n_latam_payment_mass_transfer.py
+++ addons/l10n_latam_check/wizards/l10n_latam_payment_mass_transfer.py
@@ -44,28 +44,15 @@ class L10nLatamPaymentMassTransfer(models.TransientModel):
     @api.model
     def default_get(self, fields_list):
         res = super().default_get(fields_list)
-        if self._context.get('active_model') != 'account.payment':
-            raise UserError(_("The register payment wizard should only be called on account.payment records."))
-        payments = self.env['account.payment'].browse(self._context.get('active_ids', []))
-        res['check_ids'] = payments.ids
-
-        checks = payments.filtered(lambda x: x.payment_method_line_id.code == 'new_third_party_checks')
-        journal_id = checks.l10n_latam_check_current_journal_id
-        pay_method_line = journal_id._get_available_payment_method_lines('outbound').filtered(
-            lambda x: x.code == 'out_third_party_checks')
-
-        if not all(check.state == 'posted' for check in checks):
-            raise UserError(_("All the selected checks must be posted"))
-        if len(journal_id) != 1:
-            raise UserError(_("All selected checks must be on the same journal"))
-        if not journal_id.inbound_payment_method_line_ids.filtered(
-                lambda x: x.code == 'in_third_party_checks'):
-            raise UserError(_("Checks must be on a third party checks journal to be transfered by this wizard"))
-        if not pay_method_line:
-            raise UserError(_("There is no 'Existing Third Party Checks' payment method configured on journal %s",
-                            journal_id.name))
-
-        res['journal_id'] = journal_id.id
+        if 'check_ids' in fields_list and 'check_ids' not in res:
+            if self._context.get('active_model') != 'account.payment':
+                raise UserError(_("The register payment wizard should only be called on account.payment records."))
+            checks = self.env['account.payment'].browse(self._context.get('active_ids', []))
+            if checks.filtered(lambda x: x.payment_method_line_id.code != 'new_third_party_checks'):
+                raise 'You have select some payments that are not checks. Please call this action from the Third Party Checks menu'
+            elif not all(check.state == 'posted' for check in checks):
+                raise UserError(_("All the selected checks must be posted"))
+            res['check_ids'] = checks.ids
         return res
 
     def _create_payments(self):
