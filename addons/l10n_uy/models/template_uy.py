51d91451f12dfacb62969e687b23bcdba85d7ebf
Katherine Zaoral <kz@adhoc.com.ar>
2023-10-06 10:30:20 -0300

    squash! [ADD] l10n_uy: changes from review
    - re add pos account

--- addons/l10n_uy/models/template_uy.py
+++ addons/l10n_uy/models/template_uy.py
@@ -27,6 +27,7 @@ class AccountChartTemplate(models.AbstractModel):
                 'bank_account_code_prefix': '1111',
                 'cash_account_code_prefix': '1112',
                 'transfer_account_code_prefix': '11120',
+                'account_default_pos_receivable_account_id': 'uy_code_11307',
                 'income_currency_exchange_account_id': 'uy_code_4302',
                 'expense_currency_exchange_account_id': 'uy_code_5302',
                 'account_journal_early_pay_discount_loss_account_id': 'uy_code_5303',
