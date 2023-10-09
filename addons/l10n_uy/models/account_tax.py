0a055a70e4d540f772b2bbca0659ce7ce70244fd
Katherine Zaoral <kz@adhoc.com.ar>
2023-10-09 14:01:50 -0300

    add tooltip

--- addons/l10n_uy/models/account_tax.py
+++ addons/l10n_uy/models/account_tax.py
@@ -8,4 +8,4 @@ class AccountTax(models.Model):
 
     l10n_uy_tax_category = fields.Selection([
         ('vat', 'VAT'),
-    ], string="Tax Category")
+    ], string="Tax Category", help="UY: Use to group the transactions in the Financial Reports required by DGI")
