7aeb71c5292be1c8f91df8f7f7cffcffc0eebf2b
Katherine Zaoral <kz@adhoc.com.ar>
2023-10-09 14:05:17 -0300

    remove unused imports

--- addons/l10n_uy/models/account_move.py
+++ addons/l10n_uy/models/account_move.py
@@ -1,6 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, _
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class AccountMove(models.Model):
